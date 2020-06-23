076b0d17b1b5adbfcc5406d1489b05f9af34ec00
Gowtham Katta <gowthamkatta@gmail.com>
2020-06-23 01:52:51 -0500

    Saving

--- udemy_exercises/songs/Song.java
+++ udemy_exercises/songs/Song.java
@@ -1,22 +1,6 @@
 package udemy_exercises.songs;
 
 class Song {
-    private String title;
-    private double duration;
-
-    public Song(String title, double duration) {
-        this.title = title;
-        this.duration = duration;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public String toString() {
-        return this.title + ": " + this.duration;
-    }
 
 }
 
